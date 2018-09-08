4c0ba7791c3c05f3ed58c879389ceed215da1c40
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-08 15:36:30 +0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Routes from './Routes.js';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (

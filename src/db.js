a1019615b71885778a5a428dd62075cd871054ba
Kuan Wei Heng <kuanwh1996@gmail.com>
2018-09-08 15:31:26 +0800

    fix projectId

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'https://showface-425d0.firebaseio.com/',
-  projectId: 'facetron',
+  projectId: 'showface',
 });
 
 var db = firebase.firestore();

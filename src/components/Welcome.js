8b317343f09334520833a9340621deae98a597bf
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-09 01:32:59 +0800

    set obnoxious background color to only landing page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.png';
 
 const Welcome = () => (
-  <div className="full-page flex">
+  <div id="landing-page" className="full-page flex">
     <img id="logo" src={logo} alt="logo" className="center" />
     <Link to="/create" className="btn btn-outline-primary btn-lg btn-long">
       Create New Poll

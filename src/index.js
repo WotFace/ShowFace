f81a0a0fdbda943275996157c3e46d3eb5c895ac
E-Liang Tan <eliang@eliangtan.com>
2018-09-09 04:53:03 +0800

    Remove dotenv as it's included in our existing deps

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-require('dotenv').config();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

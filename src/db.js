38b734139004828bdd237639c68ce858b707b3d0
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-08 16:49:54 +0800

    fix db

--- src/db.js
+++ src/db.js
@@ -3,9 +3,13 @@ import firebase from 'firebase';
 firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'https://showface-425d0.firebaseio.com/',
-  projectId: 'showface',
+  projectId: 'showface-425d0',
 });
 
 var db = firebase.firestore();
+
+db.settings({
+  timestampsInSnapshots: true,
+});
 
 export default db;

a6ee4811c3b9ea71afb532e096c7cc524f0321c2
Kuan Wei Heng <kuanwh1996@gmail.com>
2018-09-08 14:42:19 +0800

    Install firebase

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "https://haxaton.firebaseio.com/",
+  projectId: "haxaton"
+});
+
+var db = firebase.firestore();
+
+export default db;

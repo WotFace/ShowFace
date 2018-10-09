3805f9d74f93a5c3c88a039d07206749b5dd2cd9
E-Liang Tan <eliang@eliangtan.com>
2018-10-09 10:21:47 +0800

    Add Dashboard and Create Show button to welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -27,30 +27,38 @@ class WelcomePage extends Component {
   render() {
     const user = this.state.user;
 
-    // TODO: Redirect to user's page if logged in
-    const splashScreenButtons = user ? (
-      <div>
+    const dashboardButton = user && (
+      <>
         <h1 id="header">Welcome, {this.state.user.displayName}!</h1>
-        <Link to="/create" className="btn btn-outline-primary btn-lg btn-block">
-          Create New Poll
+        <Link to="/dashboard" className="btn btn-outline-primary btn-lg btn-block">
+          Dashboard
         </Link>
-      </div>
-    ) : (
-      <div>
+      </>
+    );
+
+    const authButtons = user || (
+      <>
         <Link to="/login" className="btn btn-outline-primary btn-lg btn-block">
           Log in
         </Link>
         <Link to="/signup" className="btn btn-outline-primary btn-lg btn-block">
           Sign up
         </Link>
-      </div>
+      </>
     );
+
     return (
       <div>
         <div id="landing-page" className="full-page flex">
           <div className="container WelcomePage-content">
             <img id="logo" src={logo} alt="logo" className="center" />
-            {splashScreenButtons}
+            <div>
+              {dashboardButton}
+              <Link to="/create" className="btn btn-outline-primary btn-lg btn-block">
+                Create Show
+              </Link>
+              {authButtons}
+            </div>
           </div>
         </div>
       </div>

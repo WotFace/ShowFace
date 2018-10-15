2becbd76194aeb22a275c9d40e34f65a9a518ada
E-Liang Tan <eliang@eliangtan.com>
2018-10-15 20:49:21 +0800

    Move new ShowResultsSidebar into new file

--- src/components/ShowResults.js
+++ src/components/ShowResults.js
@@ -1,60 +1,9 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import classnames from 'classnames';
-import _ from 'lodash';
 import Timeline from './Timeline';
-import { respondentToUserIdOrName, respondentsToDict } from '../utils/response';
+import { respondentsToDict } from '../utils/response';
 import { datesFromRange } from '../utils/datetime';
-
-import styles from './ShowResults.module.scss';
-
-function ShowAttendees({ respondents, renderableRespondents, time }) {
-  const respondersRespondentsObj = _.zipObject(
-    respondents.map(respondentToUserIdOrName),
-    respondents,
-  );
-  const responders = Object.keys(respondersRespondentsObj);
-  const respondersAtTime = new Set(renderableRespondents.get(time));
-
-  const [attending, possiblyNotAttending] = _.partition(responders, (r) => respondersAtTime.has(r));
-  // TODO: Partition possiblyNotAttending further into non-responses and not attendings
-  const notAttending = possiblyNotAttending;
-
-  // TODO: Display respondents differently depending on whether the user is
-  // logged in, has admin rights, and whether the respondent has responded
-  function renderRespondent(responder, respondent) {
-    const displayName = respondent.user ? respondent.user.name : respondent.anonymousName;
-    return <li key={responder}>{displayName}</li>;
-  }
-
-  return (
-    <div className="col-4">
-      <section className={classnames(styles.attendees, 'flex-item')}>
-        {time ? (
-          <h2 className={styles.pollTime}>{moment(time).format('Do MMM YYYY hh:mma')}</h2>
-        ) : null}
-        <section id="attending">
-          <h3>Attending</h3>
-          <ol>
-            {attending.map((responder) => {
-              const respondent = respondersRespondentsObj[responder];
-              return renderRespondent(responder, respondent);
-            })}
-          </ol>
-        </section>
-        <section id="notAttending">
-          <h3>Not Attending</h3>
-          <ol>
-            {notAttending.map((responder) => {
-              const respondent = respondersRespondentsObj[responder];
-              return renderRespondent(responder, respondent);
-            })}
-          </ol>
-        </section>
-      </section>
-    </div>
-  );
-}
+import ShowResultsSidebar from './ShowResultsSidebar';
 
 class ShowResults extends Component {
   state = {};
@@ -89,7 +38,7 @@ class ShowResults extends Component {
           maxSelectable={maxSelectable}
           onCellHover={this.handleCellHover}
         />
-        <ShowAttendees
+        <ShowResultsSidebar
           respondents={respondents}
           renderableRespondents={renderableRespondents}
           time={selectedTime}
---
+++ src/components/ShowResultsSidebar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import moment from 'moment';
+import classnames from 'classnames';
+import _ from 'lodash';
+import { respondentToUserIdOrName } from '../utils/response';
+
+import styles from './ShowResultsSidebar.module.scss';
+
+function ShowAttendees({ respondents, renderableRespondents, time }) {
+  const respondersRespondentsObj = _.zipObject(
+    respondents.map(respondentToUserIdOrName),
+    respondents,
+  );
+  const responders = Object.keys(respondersRespondentsObj);
+  const respondersAtTime = new Set(renderableRespondents.get(time));
+
+  const [attending, possiblyNotAttending] = _.partition(responders, (r) => respondersAtTime.has(r));
+  // TODO: Partition possiblyNotAttending further into non-responses and not attendings
+  const notAttending = possiblyNotAttending;
+
+  // TODO: Display respondents differently depending on whether the user is
+  // logged in, has admin rights, and whether the respondent has responded
+  function renderRespondent(responder, respondent) {
+    const displayName = respondent.user ? respondent.user.name : respondent.anonymousName;
+    return <li key={responder}>{displayName}</li>;
+  }
+
+  return (
+    <div>
+      <section className={classnames(styles.attendees, 'flex-item')}>
+        {time ? (
+          <h2 className={styles.pollTime}>{moment(time).format('Do MMM YYYY hh:mma')}</h2>
+        ) : null}
+        <section id="attending">
+          <h3>Attending</h3>
+          <ol>
+            {attending.map((responder) => {
+              const respondent = respondersRespondentsObj[responder];
+              return renderRespondent(responder, respondent);
+            })}
+          </ol>
+        </section>
+        <section id="notAttending">
+          <h3>Not Attending</h3>
+          <ol>
+            {notAttending.map((responder) => {
+              const respondent = respondersRespondentsObj[responder];
+              return renderRespondent(responder, respondent);
+            })}
+          </ol>
+        </section>
+      </section>
+    </div>
+  );
+}
+
+export default function ShowResultsSidebar({ respondents, renderableRespondents, time }) {
+  return (
+    <div className="col-4">
+      <ShowAttendees
+        respondents={respondents}
+        renderableRespondents={renderableRespondents}
+        time={time}
+      />
+    </div>
+  );
+}

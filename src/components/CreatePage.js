1f65746bc3d615b1d3074e6c372019a271018d7d
trang mai <trangmei@gmail.com>
2018-10-16 14:58:36 +0800

    Create authed Show when user is logged in

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -6,18 +6,21 @@ import classnames from 'classnames';
 import { Mutation } from 'react-apollo';
 import ReactLoading from 'react-loading';
 import gql from 'graphql-tag';
+import { auth } from '../firebase';
 
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import styles from './CreatePage.module.scss';
 
-import TextField, {Input} from '@material/react-text-field';
+import TextField, { Input } from '@material/react-text-field';
 import Button from '@material/react-button';
 
 class CreatePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: null,
+      auth: null,
       name: '',
       dateRanges: {
         selection: {
@@ -32,12 +35,25 @@ class CreatePage extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user });
+        // TODO: change to actual auth object when DO is up
+        this.setState({ auth: { uid: 'trang', token: 'stupidtoken' } });
+      } else {
+        this.setState({ user: null });
+        this.setState({ auth: null });
+      }
+    });
+  }
+
   handleSubmit(event) {
     // TODO: Add interval option to UI and retrieve from state
     const interval = 15;
-    const { name, dateRanges } = this.state;
+    const { name, dateRanges, auth } = this.state;
     const { startDate, endDate } = dateRanges.selection;
-    this.props.createShow(name, startDate, endDate, interval);
+    this.props.createShow(name, startDate, endDate, interval, auth);
     event.preventDefault();
   }
 
@@ -61,11 +77,12 @@ class CreatePage extends Component {
 
     if (loading) {
       // TODO: Beautify
-      return <section className="full-page flex">
+      return (
+        <section className="full-page flex">
           <h2>Creating</h2>
           <ReactLoading type="bubbles" color="#111" />
         </section>
-
+      );
     } else if (data) {
       return <Redirect to={`/show/${data.createNewShow.slug}`} />;
     } else {
@@ -85,16 +102,12 @@ class CreatePage extends Component {
             <div className={styles.create_page_form}>
               <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
-                  <TextField
-                    label='Enter Show Name'
-                    className={styles.form_input}
-                    outlined
-                  >
+                  <TextField label="Enter Show Name" className={styles.form_input} outlined>
                     <Input
                       type="text"
                       name="name"
                       value={this.state.name}
-                      autoComplete = "off"
+                      autoComplete="off"
                       onChange={this.handleInputChange}
                     />
                   </TextField>
@@ -133,8 +146,10 @@ const CREATE_NEW_SHOW_MUTATION = gql`
     $startDate: DateTime!
     $endDate: DateTime!
     $interval: Int!
+    $auth: AuthInput
   ) {
     createNewShow(
+      auth: $auth
       data: { name: $name, startDate: $startDate, endDate: $endDate, interval: $interval }
     ) {
       slug
@@ -147,8 +162,8 @@ export default withAlert((props) => (
     {(createNewShow, result) => (
       <CreatePage
         {...props}
-        createShow={(name, startDate, endDate, interval) =>
-          createNewShow({ variables: { name, startDate, endDate, interval } })
+        createShow={(name, startDate, endDate, interval, auth) =>
+          createNewShow({ variables: { name, startDate, endDate, interval, auth } })
         }
         createShowResult={result}
       />

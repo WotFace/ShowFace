1026474fcd227ae814a67bd3f9be8ef0fd22c071
E-Liang Tan <eliang@eliangtan.com>
2018-10-23 17:23:58 +0800

    Separate create page into section elements

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -92,44 +92,40 @@ class CreatePage extends Component {
       // TODO: Validate input and disable submit button if necessary
       return (
         <div id={styles.pageContainer}>
-          <section id={styles.form_header}>
+          <section>
             <h1 id={styles.header}>Create a new Meeting</h1>
           </section>
-          <section>
-            <div className={styles.create_page_form}>
-              <form>
-                <div className={styles.formGroup}>
-                  <TextField label="Meet for what?" className={styles.form_input} disabled>
-                    <Input
-                      type="text"
-                      name="name"
-                      value={name}
-                      autoComplete="off"
-                      onChange={this.handleInputChange}
-                    />
-                  </TextField>
-                </div>
-                <div className={styles.formGroup}>
-                  <DayPicker
-                    fromMonth={today}
-                    disabledDays={{ before: today }}
-                    selectedDays={selectedDays}
-                    onDayClick={this.handleDayClick}
-                  />
-                </div>
-              </form>
-              <BottomAppBar>
-                <Button
-                  className={styles.submitButton}
-                  onClick={this.handleSubmit}
-                  disabled={noSelectedDay || name.length === 0}
-                  raised
-                >
-                  Submit
-                </Button>
-              </BottomAppBar>
-            </div>
-          </section>
+          <form>
+            <section className={styles.formSection}>
+              <TextField label="Meet for what?" className={styles.formInput} disabled>
+                <Input
+                  type="text"
+                  name="name"
+                  value={name}
+                  autoComplete="off"
+                  onChange={this.handleInputChange}
+                />
+              </TextField>
+            </section>
+            <section className={styles.formSection}>
+              <DayPicker
+                fromMonth={today}
+                disabledDays={{ before: today }}
+                selectedDays={selectedDays}
+                onDayClick={this.handleDayClick}
+              />
+            </section>
+            <BottomAppBar>
+              <Button
+                className={styles.submitButton}
+                onClick={this.handleSubmit}
+                disabled={noSelectedDay || name.length === 0}
+                raised
+              >
+                Submit
+              </Button>
+            </BottomAppBar>
+          </form>
         </div>
       );
     }

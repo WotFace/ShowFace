a4c4614df3b077be012b01aaec3895e4fee10ca4
Anthony Tantra <tantra.anthony@gmail.com>
2018-10-23 22:40:16 +0800

    bottom bar

--- src/components/ShowResults.js
+++ src/components/ShowResults.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Timeline from './Timeline';
 import { respondentsToDict } from '../utils/response';
 import ShowResultsSidebar from './ShowResultsSidebar';
+import BottomBar from './ShowResultsBottomBar';
 import styles from './ShowResults.module.scss';
 
 class ShowResults extends Component {
@@ -28,25 +29,33 @@ class ShowResults extends Component {
     // TODO: Pass startTime and endTime in from show, and make Timeline handle them.
     // TODO: Deduplicate dates, startTime, endTime between ShowResults and ShowRespond
     return (
-      <div className={styles.resultsContainer}>
-        <Timeline
-          className={styles.timeline}
-          dates={dates}
-          startTime={startTime}
-          endTime={endTime}
-          interval={interval}
-          respondents={respondents}
-          maxSelectable={maxSelectable}
-          onCellHover={this.handleCellHover}
-        />
-        <div>
-          <ShowResultsSidebar
-            className={styles.sidebar}
+      <div>
+        <div className={styles.resultsContainer}>
+          <Timeline
+            className={styles.timeline}
+            dates={dates}
+            startTime={startTime}
+            endTime={endTime}
+            interval={interval}
             respondents={respondents}
-            renderableRespondents={renderableRespondents}
-            time={selectedTime}
+            maxSelectable={maxSelectable}
+            onCellHover={this.handleCellHover}
           />
+          <div>
+            <ShowResultsSidebar
+              className={styles.sidebar}
+              respondents={respondents}
+              renderableRespondents={renderableRespondents}
+              time={selectedTime}
+            />
+          </div>
         </div>
+        <BottomBar
+          className={styles.bottomBar}
+          respondents={respondents}
+          renderableRespondents={renderableRespondents}
+          time={selectedTime}
+        />
       </div>
     );
   }

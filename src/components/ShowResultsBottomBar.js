1f3c5344773a4f13d3ba85155f3632b1dd76c555
Anthony Tantra <tantra.anthony@gmail.com>
2018-10-24 00:36:20 +0800

    some fixes

--- src/components/ShowResultsBottomBar.js
+++ src/components/ShowResultsBottomBar.js
@@ -15,7 +15,9 @@ function renderShortMessage(time, attending, notAttending) {
   notAttendingCount = notAttending.length === 0 ? 0 : notAttending.length;
 
   return (
-    <div className={styles.header}>{format(time, 'Do MMM YYYY hh:mma')} <br /> {attendingCount} attending, {notAttendingCount} not attending</div>
+    <div className={styles.header}>
+      {format(time, 'Do MMM YYYY hh:mma')} <br /> {attendingCount} attending, {notAttendingCount} not attending
+    </div>
   );
 }
 
@@ -25,7 +27,7 @@ function renderRespondent(responder, respondent) {
     <div className={styles.respondents} key={responder}>
       {displayName}
       <MaterialIcon
-        icon="visibility"
+        icon="delete"
         className={styles.interactives}
         hasRipple={true}
         onClick={() => {}}
@@ -80,13 +82,15 @@ class BottomBar extends Component {
             (isClosed ? styles.slideBackward : null),
           )}
         >
-          <MaterialIcon
-            icon={isOpen ? "clear" : "more_vert"}
-            className={classnames(styles.icon, isOpen ? styles.rotateOut : null, isClosed ? styles.rotateIn : null)}
-            hasRipple={true}
-            onClick={this.handleOpen}
-          />
-          {renderShortMessage(time, attending, notAttending)}
+          <div className={isOpen ? styles.sticky : null}>
+            <MaterialIcon
+              icon={isOpen ? "clear" : "more_vert"}
+              className={classnames(styles.icon, isOpen ? styles.rotateOut : null, isClosed ? styles.rotateIn : null)}
+              hasRipple={true}
+              onClick={this.handleOpen}
+            />
+            {renderShortMessage(time, attending, notAttending)}
+          </div>
           <div className={styles.scrollable}>
             <section className={styles.attendees}>
               <h3>Attending</h3>
@@ -110,66 +114,3 @@ class BottomBar extends Component {
 }
 
 export default BottomBar;
-
-// function ShowAttendees({ respondents, renderableRespondents, time }) {
-//   const respondersRespondentsObj = _.zipObject(
-//     respondents.map(respondentToEmailOrName),
-//     respondents,
-//   );
-//   const responders = Object.keys(respondersRespondentsObj);
-//   const respondersAtTime = new Set(renderableRespondents.get(time));
-
-//   const [attending, possiblyNotAttending] = _.partition(responders, (r) => respondersAtTime.has(r));
-//   // TODO: Partition possiblyNotAttending further into non-responses and not attendings
-//   const notAttending = possiblyNotAttending;
-
-//   // TODO: Display respondents differently depending on whether the user is
-//   // logged in, has admin rights, and whether the respondent has responded
-//   function renderRespondent(responder, respondent) {
-//     const displayName = respondent.user ? respondent.user.name : respondent.anonymousName;
-//     return <li key={responder}>{displayName}</li>;
-//   }
-
-//   return (
-//     <div>
-//       <section className={classnames(styles.attendees, 'flex-item')}>
-//         {time ? <h2 className={styles.pollTime}>{format(time, 'Do MMM YYYY hh:mma')}</h2> : null}
-//         <section id="attending">
-//           <h3>Attending</h3>
-//           <ol>
-//             {attending.map((responder) => {
-//               const respondent = respondersRespondentsObj[responder];
-//               return renderRespondent(responder, respondent);
-//             })}
-//           </ol>
-//         </section>
-//         <section id="notAttending">
-//           <h3>Not Attending</h3>
-//           <ol>
-//             {notAttending.map((responder) => {
-//               const respondent = respondersRespondentsObj[responder];
-//               return renderRespondent(responder, respondent);
-//             })}
-//           </ol>
-//         </section>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default function ShowResultsBottomBar({
-//   className,
-//   respondents,
-//   renderableRespondents,
-//   time,
-// }) {
-//   return (
-//     <div className={className}>
-//       <ShowAttendees
-//         respondents={respondents}
-//         renderableRespondents={renderableRespondents}
-//         time={time}
-//       />
-//     </div>
-//   );
-// }

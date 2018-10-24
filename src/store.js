5a1ff6d245908cf3bfc64eff753defa90ae97780
E-Liang Tan <eliang@eliangtan.com>
2018-10-24 12:17:33 +0800

    Add redux-persist

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { createStore, compose } from 'redux';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 import rootReducer from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers());
+const store = createStore(persistedReducer, composeEnhancers());
 
 // Enable webpack hot module replacement for reducers
 if (module.hot && process.env.NODE_ENV !== 'production') {
@@ -12,3 +19,4 @@ if (module.hot && process.env.NODE_ENV !== 'production') {
 }
 
 export default store;
+export const persistor = persistStore(store);

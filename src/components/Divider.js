712ed028b8657cc8d89cdb6a379ea86f58aa4af1
E-Liang Tan <eliang@eliangtan.com>
2018-10-24 18:50:00 +0800

    Add support for text in Divider

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -2,6 +2,18 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Divider.module.scss';
 
-export default function Divider({ className, ...otherProps }) {
-  return <hr className={classnames(className, styles.divider)} {...otherProps} />;
+function DividerLine({ className, ...otherProps }) {
+  return <hr className={classnames(className, styles.dividerLine)} {...otherProps} />;
+}
+
+export default function Divider({ text, ...otherProps }) {
+  if (!text) return <DividerLine {...otherProps} />;
+  const { className, ...otherOtherProps } = otherProps;
+  return (
+    <div {...otherOtherProps} className={classnames(className, styles.divider)}>
+      <DividerLine />
+      <span className={classnames(styles.text, 'mdc-typography--overline')}>{text}</span>
+      <DividerLine />
+    </div>
+  );
 }

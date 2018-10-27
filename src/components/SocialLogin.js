a625b517db42792541ac88e96a3f97071e615de8
trang mai <trangmei@gmail.com>
2018-10-27 13:08:39 +0800

    Query prisma user for social authed users

--- src/components/SocialLogin.js
+++ src/components/SocialLogin.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
+import { ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
 import { withAlert } from 'react-alert';
 import { facebookAuthProvider, googleAuthProvider, auth } from '../firebase';
+import { getAuthInput } from '../utils/auth';
 
 class SocialLogin extends Component {
   constructor(props) {
     super(props);
-    this.state = { user: null };
     this.socialAuth = this.socialAuth.bind(this);
   }
 
-  socialAuth(provider) {
+  socialAuth(provider, client) {
+    const fragments = {
+      user: gql`
+        fragment LoginPage on User {
+          id
+          uid
+          email
+          name
+          isPremium
+          createdAt
+        }
+      `,
+    };
+
+    const GET_USER_QUERY = gql`
+      query user($auth: AuthInput!) {
+        user(auth: $auth) {
+          ...LoginPage
+        }
+      }
+      ${fragments.user}
+    `;
     auth()
       .signInWithPopup(provider)
-      .then((result) => {
-        console.log('Credentials', result);
-        this.setState({ user: result });
+      .then(async (result) => {
+        const auth = await getAuthInput();
+        const { data } = await client.query({
+          query: GET_USER_QUERY,
+          variables: { auth: auth },
+        });
+        console.log(data.user);
       })
       .catch((error) => {
         console.log('Error', error);
       });
-    console.log('Authenticating with Zuckerberg', this.state.user);
   }
 
   render() {
     return (
-      <div>
-        <button
-          style={{ width: '100%', marginBottom: '10px' }}
-          className="btn btn-outline-primary btn-lg btn-block"
-          onClick={() => this.socialAuth(googleAuthProvider)}
-        >
-          Log in with Google
-        </button>
-        <button
-          style={{ width: '100%' }}
-          className="btn btn-outline-primary btn-lg btn-block"
-          onClick={() => this.socialAuth(facebookAuthProvider)}
-        >
-          Log in with Facebook
-        </button>
-        <hr style={{ marginTop: '10px', marginBottom: '10px' }} />
-      </div>
+      <ApolloConsumer>
+        {(client) => (
+          <div>
+            <button
+              style={{ width: '100%', marginBottom: '10px' }}
+              className="btn btn-outline-primary btn-lg btn-block"
+              onClick={() => this.socialAuth(googleAuthProvider, client)}
+            >
+              Log in with Google
+            </button>
+            <button
+              style={{ width: '100%' }}
+              className="btn btn-outline-primary btn-lg btn-block"
+              onClick={() => this.socialAuth(facebookAuthProvider, client)}
+            >
+              Log in with Facebook
+            </button>
+            <hr style={{ marginTop: '10px', marginBottom: '10px' }} />
+          </div>
+        )}
+      </ApolloConsumer>
     );
   }
 }

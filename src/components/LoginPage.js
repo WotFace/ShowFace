4e70683a77afeb476edf89f8a942d88abdf2dcd3
trang mai <trangmei@gmail.com>
2018-10-28 23:45:10 +0800

    Render SignupForm as a tab in LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { withAlert } from 'react-alert';
 import { auth } from '../firebase';
 import SocialLogin from './SocialLogin';
+import SignupForm from './SignupForm';
+import Tab from '@material/react-tab';
+import TabBar from '@material/react-tab-bar';
+import styles from './ShowPage.module.scss';
 
 class LoginPage extends Component {
   constructor(props) {
     super(props);
-    this.state = { user: null };
+    this.state = { selectedTab: 0 };
     this.authWithEmailPassword = this.authWithEmailPassword.bind(this);
   }
 
@@ -16,73 +20,83 @@ class LoginPage extends Component {
     event.preventDefault();
     auth()
       .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        console.log('Credentials', result);
-        this.setState({ user: result });
-      })
       .catch((error) => {
         console.log('Error', error);
       });
-    console.log('Authenticating with Firebase');
-    console.table([
-      {
-        email: this.emailInput.value,
-        password: this.passwordInput.value,
-      },
-    ]);
+  }
+
+  changeTab(activeIndex) {
+    this.setState({ selectedTab: activeIndex });
   }
 
   render() {
-    return !auth().currentUser ? (
+    const { selectedTab } = this.state;
+    return (
       <div className="container Welcome-content">
         <section id="form-header">
           <h1 id="header">Log in</h1>
         </section>
         <section id="form" className="row">
           <div className="col">
-            <div>
-              <SocialLogin />
-              <div
-                ref={(form) => {
-                  this.loginForm = form;
-                }}
-              >
-                <input
-                  style={{ width: '100%', margin: '10px auto' }}
-                  className="form-control"
-                  name="email"
-                  type="email"
-                  ref={(input) => {
-                    this.emailInput = input;
-                  }}
-                  placeholder="Email"
-                />
-                <input
-                  style={{ width: '100%', margin: '10px auto' }}
-                  className="form-control"
-                  name="password"
-                  type="password"
-                  ref={(input) => {
-                    this.passwordInput = input;
-                  }}
-                  placeholder="Password"
-                />
-                <button
-                  style={{ width: '100%' }}
-                  className="btn btn-outline-primary btn-lg btn-block"
-                  value="Log in"
-                  onClick={(event) => {
-                    this.authWithEmailPassword(event);
+            <TabBar
+              className={styles.tabBar}
+              activeIndex={selectedTab}
+              handleActiveIndexUpdate={(activeIndex) => this.changeTab(activeIndex)}
+            >
+              <Tab key="Login">
+                <span className="mdc-tab__text-label">Log in</span>
+              </Tab>
+              <Tab key="Signup">
+                <span className="mdc-tab__text-label">Sign up</span>
+              </Tab>
+            </TabBar>
+            {selectedTab === 0 ? (
+              <div>
+                <SocialLogin />
+                <div
+                  ref={(form) => {
+                    this.loginForm = form;
                   }}
                 >
-                  Log in
-                </button>
+                  <input
+                    style={{ width: '100%', margin: '10px auto' }}
+                    className="form-control"
+                    name="email"
+                    type="email"
+                    ref={(input) => {
+                      this.emailInput = input;
+                    }}
+                    placeholder="Email"
+                  />
+                  <input
+                    style={{ width: '100%', margin: '10px auto' }}
+                    className="form-control"
+                    name="password"
+                    type="password"
+                    ref={(input) => {
+                      this.passwordInput = input;
+                    }}
+                    placeholder="Password"
+                  />
+                  <button
+                    style={{ width: '100%' }}
+                    className="btn btn-outline-primary btn-lg btn-block"
+                    value="Log in"
+                    onClick={(event) => {
+                      this.authWithEmailPassword(event);
+                    }}
+                  >
+                    Log in
+                  </button>
+                </div>
               </div>
-            </div>
+            ) : (
+              <SignupForm />
+            )}
           </div>
         </section>
       </div>
-    ) : null;
+    );
   }
 }
 

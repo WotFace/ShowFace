14fa3527c9a91b258ee7264c5f0cf4b195ea3a1e
Anthony Tantra <tantra.anthony@gmail.com>
2018-10-29 16:51:06 +0800

    background added and supports multiple screen sizes

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import QuickCreate from './QuickCreate';
+import Card from '@material/react-card';
+
 import styles from './WelcomePage.module.scss';
 
 class WelcomePage extends Component {
   render() {
     return (
-      <div id={styles.halfPage}>
-        <div id={styles.titleContainer}>
-          <h1 id={styles.pageTitle}>Welcome to ShowFace!</h1>
-          <h2 id={styles.subText}>We help you navigate schedules to find the best time to meet.</h2>
+      <div className={styles.background}>
+        <div id={styles.halfPage}>
+          <div id={styles.titleContainer}>
+            <h1 id={styles.pageTitle}>Welcome to ShowFace!</h1>
+            <h2 id={styles.subText}>We help you navigate schedules to find the best time to meet.</h2>
+          </div>
+          <QuickCreate />
         </div>
-        <QuickCreate />
       </div>
     );
   }

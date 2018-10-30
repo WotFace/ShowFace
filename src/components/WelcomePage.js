577e92f31af6d7eb3723a4bb5d0f588d4c9299a3
Anthony Tantra <tantra.anthony@gmail.com>
2018-10-30 18:58:43 +0800

    added nav link to landing page ready section

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import QuickCreate from './QuickCreate';
 import Card from '@material/react-card';
+import { NavLink } from 'react-router-dom';
 import MaterialIcon from '@material/react-material-icon';
 import Button from '@material/react-button';
+import classnames from 'classnames';
 
 import styles from './WelcomePage.module.scss';
 
@@ -23,17 +25,17 @@ class WelcomePage extends Component {
         <div className={styles.descriptionContainer}>
           <div className={styles.descriptionItems}>
             <MaterialIcon icon="bar_chart" className={styles.descriptionIcon} />
-            <h2>Beautiful Visualizations</h2>
+            <h2 className={styles.descriptionItemsHeader}>Beautiful Visualizations</h2>
             <h2>View everyone's schedules in a timetable-style heatmap. Immediately see who is busy at any particular time and easily identify the timeslots with the highest availability!</h2>
           </div>
           <div className={styles.descriptionItems}>
             <MaterialIcon icon="dashboard" className={styles.descriptionIcon} />
-            <h2>Intuitive Interface</h2>
+            <h2 className={styles.descriptionItemsHeader}>Intuitive Interface</h2>
             <h2>Our drag-to-highlight feature provides you with the efficiency of painting over large swaths of the timetable, yet retaining the precision required to indicate single slots.</h2>
           </div>
           <div className={styles.descriptionItems}>
             <MaterialIcon icon="phonelink" className={styles.descriptionIcon} />
-            <h2>Powerful Functionality</h2>
+            <h2 className={styles.descriptionItemsHeader}>Powerful Functionality</h2>
             <h2>Get the best suggested meeting time, send automated reminders, share calendar invites! Organizing a meeting has never been easier!</h2>
           </div>
         </div>
@@ -43,8 +45,12 @@ class WelcomePage extends Component {
             <h2>Create an account or start now</h2>
           </div>
           <div className={styles.readyColumnButton}>
-            <Button className={styles.readyButton} raised onClick={() => {}}>CREATE AN ACCOUNT</Button>
-            <Button outlined onClick={() => {}}>CREATE A NEW POLL</Button>
+            <NavLink className={styles.navLink} to={{ pathname: '/login' }}>
+              <Button className={classnames(styles.readyButton, styles.readyButtonSignUp)} raised>CREATE AN ACCOUNT</Button>
+            </NavLink>
+            <NavLink className={styles.navLink} to={{ pathname: '/new' }}>
+              <Button className={styles.readyButton} outlined>CREATE A NEW POLL</Button>
+            </NavLink>
           </div>
         </div>
         <footer className={styles.footerContainer}>

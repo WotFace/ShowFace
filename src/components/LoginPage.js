0ff34a0aa7cb4e8cc8832a7bd0faffda5ff628ce
E-Liang Tan <eliang@eliangtan.com>
2018-10-31 13:28:18 +0800

    Combine SignupForm into LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { withAlert } from 'react-alert';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Button from '@material/react-button';
 import Card from '@material/react-card';
@@ -9,34 +11,86 @@ import TabBar from '@material/react-tab-bar';
 import TextField, { Input } from '@material/react-text-field';
 
 import { auth } from '../firebase';
+import { getAuthInput } from '../utils/auth';
 import SocialLogin from './SocialLogin';
-import SignupForm from './SignupForm';
 
 import styles from './LoginPage.module.scss';
 
 class LoginPage extends Component {
+  LOGIN_TAB_IDX = 0;
+
   state = {
     selectedTab: 0,
+    nameInput: '',
     emailInput: '',
     passwordInput: '',
   };
 
-  authWithEmailPassword = (e) => {
-    e.preventDefault();
+  authWithEmailPassword() {
     const { emailInput, passwordInput } = this.state;
     auth()
       .signInWithEmailAndPassword(emailInput, passwordInput)
+      .then((res) => {
+        console.log('Logged in', res, emailInput);
+      })
       .catch((error) => {
         console.log('Error', error);
       });
+  }
+
+  createUserWithEmailAndPassword() {
+    const { nameInput, emailInput, passwordInput } = this.state;
+    auth()
+      .createUserWithEmailAndPassword(emailInput, passwordInput)
+      .then(() => this.props.createUser(nameInput, emailInput))
+      .then((res) => {
+        console.log('Signed up!', res, nameInput, emailInput);
+      })
+      .catch((error) => {
+        console.log('Error', error);
+      });
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.selectedTab === this.LOGIN_TAB_IDX) {
+      this.authWithEmailPassword();
+    } else {
+      this.createUserWithEmailAndPassword();
+    }
   };
 
   handleTabChange = (selectedTab) => this.setState({ selectedTab });
+  handleNameInputChange = (e) => this.setState({ nameInput: e.target.value });
   handleEmailInputChange = (e) => this.setState({ emailInput: e.target.value });
   handlePasswordInputChange = (e) => this.setState({ passwordInput: e.target.value });
 
   render() {
-    const { selectedTab, emailInput, passwordInput } = this.state;
+    const { selectedTab, nameInput, emailInput, passwordInput } = this.state;
+
+    const submitButton =
+      selectedTab === this.LOGIN_TAB_IDX ? (
+        <Button
+          type="submit"
+          className={styles.submitButton}
+          icon={<MaterialIcon icon="exit_to_app" />}
+          disabled={emailInput.length === 0 || passwordInput.length === 0}
+          raised
+        >
+          Log in
+        </Button>
+      ) : (
+        <Button
+          type="submit"
+          className={styles.submitButton}
+          icon={<MaterialIcon icon="account_circle" />}
+          disabled={nameInput.length === 0 || emailInput.length === 0 || passwordInput.length === 0}
+          raised
+        >
+          Sign up
+        </Button>
+      );
+
     return (
       <div className={styles.outerContainer}>
         <div className={styles.innerContainer}>
@@ -52,37 +106,35 @@ class LoginPage extends Component {
                 <span className="mdc-tab__text-label">Sign up</span>
               </Tab>
             </TabBar>
-            {selectedTab === 0 ? (
-              <form className={styles.form} onSubmit={this.authWithEmailPassword}>
-                <TextField label="Email" className={styles.formInput}>
+            <form className={styles.form} onSubmit={this.handleFormSubmit}>
+              {selectedTab !== this.LOGIN_TAB_IDX && (
+                <TextField label="Name" className={styles.formInput}>
                   <Input
-                    name="email"
-                    type="email"
-                    value={emailInput}
-                    onChange={this.handleEmailInputChange}
+                    name="name"
+                    type="name"
+                    value={nameInput}
+                    onChange={this.handleNameInputChange}
                   />
                 </TextField>
-                <TextField label="Password" className={styles.formInput}>
-                  <Input
-                    name="password"
-                    type="password"
-                    value={passwordInput}
-                    onChange={this.handlePasswordInputChange}
-                  />
-                </TextField>
-                <Button
-                  type="submit"
-                  className={styles.submitButton}
-                  icon={<MaterialIcon icon="send" />}
-                  disabled={emailInput.length === 0 || passwordInput.length === 0}
-                  raised
-                >
-                  Log in
-                </Button>
-              </form>
-            ) : (
-              <SignupForm />
-            )}
+              )}
+              <TextField label="Email" className={styles.formInput}>
+                <Input
+                  name="email"
+                  type="email"
+                  value={emailInput}
+                  onChange={this.handleEmailInputChange}
+                />
+              </TextField>
+              <TextField label="Password" className={styles.formInput}>
+                <Input
+                  name="password"
+                  type="password"
+                  value={passwordInput}
+                  onChange={this.handlePasswordInputChange}
+                />
+              </TextField>
+              {submitButton}
+            </form>
           </Card>
         </div>
       </div>
@@ -90,4 +142,25 @@ class LoginPage extends Component {
   }
 }
 
-export default withAlert(LoginPage);
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser($name: String!, $email: String!, $auth: AuthInput!) {
+    createUser(auth: $auth, data: { name: $name, email: $email }) {
+      id
+    }
+  }
+`;
+
+export default withAlert((props) => (
+  <Mutation mutation={CREATE_USER_MUTATION}>
+    {(createUser, result) => (
+      <LoginPage
+        {...props}
+        createUser={async (name, email) => {
+          const auth = await getAuthInput();
+          createUser({ variables: { name, email, auth } });
+        }}
+        createUserResult={result}
+      />
+    )}
+  </Mutation>
+));

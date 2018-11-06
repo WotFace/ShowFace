3c49f5700dec9473ca4ff5cc33662a028ac56f66
E-Liang Tan <eliang@eliangtan.com>
2018-11-06 12:25:50 +0800

    Replace CRA1 SW with CRA2's

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 // Polyfills
 import 'pepjs';
 
 const rootEl = document.getElementById('root');
 ReactDOM.render(<App />, rootEl);
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
 
 if (module.hot) {
   module.hot.accept('./App', () => {

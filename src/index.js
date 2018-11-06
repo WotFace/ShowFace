f68cf4e16ea676cb1b6cb07ff4bda5d173c397b6
E-Liang Tan <eliang@eliangtan.com>
2018-11-06 16:48:08 +0800

    Move store creation to index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 // Polyfills
 import 'pepjs';
 
 const rootEl = document.getElementById('root');
-ReactDOM.render(<App />, rootEl);
+
+function renderApp(App) {
+  ReactDOM.render(<App store={store} />, rootEl);
+}
+renderApp(App);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+// TODO: Provide config dict with onUpdate callback
 serviceWorker.register();
 
 if (module.hot) {
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
-    ReactDOM.render(<NextApp />, rootEl);
+    renderApp(NextApp);
   });
 }

3bf200f2ff4e07c93d40777cac4d5998abc4e764
E-Liang Tan <eliang@eliangtan.com>
2018-11-20 17:13:07 +0800

    Code split by routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import { Redirect, Switch, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import AppBar from './components/AppBar';
 import GoogleAnalytics from './components/GoogleAnalytics';
+import Loading from './components/errorsLoaders/Loading';
 
-import ShowPage from './components/ShowPage';
-import WelcomePage from './components/WelcomePage';
-import CreatePage from './components/CreatePage';
-import LoginPage from './components/auth/LoginPage';
-import DashboardPage from './components/DashboardPage';
-import NotFoundPage from './components/errorsLoaders/NotFoundPage';
+const ShowPage = lazy(() => import('./components/ShowPage'));
+const WelcomePage = lazy(() => import('./components/WelcomePage'));
+const CreatePage = lazy(() => import('./components/CreatePage'));
+const LoginPage = lazy(() => import('./components/auth/LoginPage'));
+const DashboardPage = lazy(() => import('./components/DashboardPage'));
+const NotFoundPage = lazy(() => import('./components/errorsLoaders/NotFoundPage'));
 
 const Routes = () => (
   <>
     <AppBar pathBlacklist={['/login', '/signup']} />
-    <Switch>
-      <Redirect exact from="/poll" to="/" />
-      <Route exact path="/" component={WelcomePage} />
-      <Route exact path="/meeting/:showId/(respond|results|settings)?" component={ShowPage} />
-      <Route exact path="/new" component={CreatePage} />
-      <Route exact path="/(login|signup)" component={LoginPage} />
-      <Route exact path="/dashboard" component={DashboardPage} />
-      <Route path="*" component={NotFoundPage} />
-    </Switch>
+    <Suspense fallback={<Loading />}>
+      <Switch>
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/meeting/:showId/(respond|results|settings)?" component={ShowPage} />
+        <Route exact path="/new" component={CreatePage} />
+        <Route exact path="/(login|signup)" component={LoginPage} />
+        <Route exact path="/dashboard" component={DashboardPage} />
+        <Route path="*" component={NotFoundPage} />
+      </Switch>
+    </Suspense>
     <GoogleAnalytics />
   </>
 );
